fb706b2642999504075a44e6ba75051a27a206b5
leocassiosilva <leocassiosilva1234@gmail.com>
2021-06-29 16:21:43 -0300

    inserindo img

--- App.js
+++ App.js
@@ -1,13 +1,45 @@
 import React, {Component} from "react"
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'; 
 class App extends Component{
+
     render(){
+
+        let nome = 'Leocassio';
+
         return(
             <View>
-                <Text>Olá Muffffdo!</Text>
-            </View>
+                <Text>Olá Mundo!!!!</Text>
+                <Text>Meu Quarto App!!!</Text>    
+                <Text style={{ color: 'red', fontSize: 25, margin: 15 }}>
+                    Programar é Bom demais!
+                </Text>
+
+        <Text style={{ fontSize: 50 }}> {nome} </Text>
+
+        <Jobs largura={10} altura={200} fulano="Jobs" />
+
+      </View>
         );
     }
 }
 
-export default App 
+export default App;
+
+class Jobs extends Component{
+    render(){
+        
+    
+        //fonte:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fbr.depositphotos.com%2Fstock-photos%2Ffutebol.html&psig=AOvVaw0T-Ggt2XkWBELtqhAWZ99h&ust=1625080709827000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCJjTscTHvfECFQAAAAAdAAAAABAN';
+
+      let img = './img.jpg'
+      return(
+        <View>
+          <Image 
+            source={{ uri: img }}
+            style={{ width: this.props.largura, height: this.props.altura }}
+        />
+        <Text> {this.props.fulano} </Text>
+       </View>
+      );
+    }
+  }
